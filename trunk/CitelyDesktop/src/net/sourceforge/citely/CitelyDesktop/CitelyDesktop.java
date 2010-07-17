b428f16e690e8e16565bd0f33d914d6214debba5
gregdavisfromnj <gregdavisfromnj@aa5927d8-7e9b-4a37-a5e5-75a576fc9da1>
2010-07-17 02:56:43 +0000

    Expanded the Desktop GUI a bit more, adding a menubar and a placeholder for the main workspace

--- trunk/CitelyDesktop/src/net/sourceforge/citely/CitelyDesktop/CitelyDesktop.java
+++ trunk/CitelyDesktop/src/net/sourceforge/citely/CitelyDesktop/CitelyDesktop.java
@@ -19,6 +19,7 @@ package net.sourceforge.citely.CitelyDesktop;
 
 import java.io.InputStream;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.*;
 
@@ -27,15 +28,19 @@ import org.eclipse.swt.widgets.*;
  */
 public class CitelyDesktop {
 
+	static Display display = null;
+	static Shell shell = null;
+	
 	/**
 	 * This is the main class which starts up a main shell window.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// setup the main window
-		Display display = new Display ();
-		Shell shell = new Shell(display);
-		
+		display = new Display ();
+		shell = new Shell(display);
+		shell.setText("Citely");
+
 		// setup the window's icon
 		Class<CitelyDesktop> desktopClass = CitelyDesktop.class;
 		ClassLoader desktopClassLoader = desktopClass.getClassLoader();
@@ -46,17 +51,66 @@ public class CitelyDesktop {
 		}
 
 		// setup other window properties
-		shell.setText("Citely");
+		Rectangle displayRect = Display.getCurrent().getClientArea();
+		shell.setSize((int)(displayRect.width *  0.61803),
+				(int)(displayRect.height * 0.61803));
 		
-		// go go go!
+		// add a menubar
+		Menu mainMenu = new Menu(shell, SWT.BAR);
+	    Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+	    Menu editMenu = new Menu(shell, SWT.DROP_DOWN);
+	    Menu helpMenu = new Menu(shell, SWT.DROP_DOWN);
+		
+		MenuItem fileMenuHeader = new MenuItem(mainMenu, SWT.CASCADE);
+	    fileMenuHeader.setText("&File");
+	    fileMenuHeader.setMenu(fileMenu);
+	
+	    MenuItem fileNewMenuItem = new MenuItem(fileMenu, SWT.PUSH);
+	    fileNewMenuItem.setText("&New");
+	    
+	    MenuItem fileExitItem = new MenuItem(fileMenu, SWT.PUSH);
+	    fileExitItem.setText("E&xit");
+	
+	    MenuItem editMenuHeader = new MenuItem(mainMenu, SWT.CASCADE);
+	    editMenuHeader.setText("&Edit");
+	    editMenuHeader.setMenu(editMenu);
+	
+	    MenuItem editDeleteItem = new MenuItem(editMenu, SWT.PUSH);
+	    editDeleteItem.setText("&Delete");
+	    
+	    MenuItem editCutItem = new MenuItem(editMenu, SWT.PUSH);
+	    editCutItem.setText("Cu&t");
+	    
+	    MenuItem editCopyItem = new MenuItem(editMenu, SWT.PUSH);
+	    editCopyItem.setText("&Copy");
+	    
+	    MenuItem editPasteItem = new MenuItem(editMenu, SWT.PUSH);
+	    editPasteItem.setText("&Paste");
+	    
+	    MenuItem helpMenuHeader = new MenuItem(mainMenu, SWT.CASCADE);
+	    helpMenuHeader.setText("&Help");
+	    helpMenuHeader.setMenu(helpMenu);
+	
+	    MenuItem helpAboutItem = new MenuItem(helpMenu, SWT.PUSH);
+	    helpAboutItem.setText("&About");
+			
+	    shell.setMenuBar(mainMenu);
+	    
+	    // add a dummy text area
+	    Text textArea = new Text(shell, SWT.CENTER | SWT.WRAP | SWT.MULTI);
+	    textArea.setBounds(shell.getClientArea());
+	    
+	    // go go go!
+		shell.pack();
 		shell.open ();
 		while (!shell.isDisposed ()) {
-			if (!display.readAndDispatch ()) display.sleep ();
+			if (!display.readAndDispatch ()) {
+				display.sleep ();
+			}
 		}
 		display.dispose ();
 	
 	}
-	
-	
-
 }
+
+
